cf8b72944ce49033b57df45420c5cacab7b1e14c
TemKey <ava-art@mail.ru>
2022-07-13 23:41:26 +0300

    fix whatsUp parser

--- WhatsUp.py
+++ WhatsUp.py
@@ -22,26 +22,30 @@ def connectwp(phones):
             inp_xpath_search = "//div[@title='Текстовое поле поиска']"
             input_box_search = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(inp_xpath_search))
             input_box_search.click()
-            time.sleep(1)
+            #time.sleep(1)
             for x in range(12):
                 input_box_search.send_keys(Keys.BACK_SPACE)
             input_box_search.send_keys(phon)
             # Click in chats
-            time.sleep(1)
-            chats = "//div[@data-testid='cell-frame-container']"
-            selected_contact = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(chats))
-            chats = "//div[@data-testid='cell-frame-container']"
-            selected_contact = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(chats))
+            #time.sleep(1)
+            try:
+                error = "//div[@class='f8jlpxt4 r5qsrrlp hp667wtd']//span"
+                error = WebDriverWait(driver, 1).until(lambda driver: driver.find_element_by_xpath(error))
+                if error.text == "Чаты, контакты и сообщения не найдены":
+                    continue
+            except: pass
+            chats = "//div[@style='z-index: 0; transition: none 0s ease 0s; height: 72px; transform: translateY(72px);']"
+            selected_contact = WebDriverWait(driver, 2).until(lambda driver: driver.find_element_by_xpath(chats))
             selected_contact.click()
             # click person info
-            time.sleep(1)
-            pers = "//div[@data-testid='conversation-info-header']"
-            selected_contact = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(pers))
+            #time.sleep(1)
+            pers = "//div[@class='_2YnE3']"
+            selected_contact = WebDriverWait(driver, 2).until(lambda driver: driver.find_element_by_xpath(pers))
             selected_contact.click()
             # get Image
-            time.sleep(1)
-            icon = "//img[@class='_8hzr9 M0JmA i0jNr']"
-            selected_contact = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(icon))
+            #time.sleep(1)
+            icon = "//div[@style='height: 200px; width: 200px; cursor: pointer;']//img[@class='_8hzr9 M0JmA i0jNr']"
+            selected_contact = WebDriverWait(driver, 2).until(lambda driver: driver.find_element_by_xpath(icon))
             url = selected_contact.get_attribute("src")
             resp = req.get(url)
             with open("images/WhastUp/"+phon+".jpg", "wb") as file:
