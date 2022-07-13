4925b7178c4e920225058f56b58dea91193b4bb5
TemKey <ava-art@mail.ru>
2022-07-13 19:18:41 +0300

    Add whatsUp parser

--- WhatsUp.py
+++ WhatsUp.py
@@ -4,27 +4,48 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import requests as req
 import time
 
-contact = "John"
+# phones = ["+79217714383", "+79954475361", "+79999836841", "+79814465925", "+79990886311"]
 
-text = "Hey, this message was sent using Selenium"
-driver = webdriver.Firefox()
-driver.get("https://web.whatsapp.com")
-print("Scan QR Code, And then Enter")
-input()
-print("Logged In")
-inp_xpath_search = "//input[@title='Search or start new chat']"
-input_box_search = WebDriverWait(driver, 50).until(lambda driver: driver.find_element_by_xpath(inp_xpath_search))
-input_box_search.click()
-time.sleep(2)
-input_box_search.send_keys(contact)
-time.sleep(2)
-selected_contact = driver.find_element_by_xpath("//span[@title='"+contact+"']")
-selected_contact.click()
-inp_xpath = '//div[@class="_2S1VP copyable-text selectable-text"][@contenteditable="true"][@data-tab="1"]'
-input_box = driver.find_element_by_xpath(inp_xpath)
-time.sleep(2)
-input_box.send_keys(text + Keys.ENTER)
-time.sleep(2)
-driver.quit()
+def connectwp(phones):
+    driver = webdriver.Firefox()
+    driver.get("https://web.whatsapp.com")
+    # print("Scan QR Code, And then Enter")
+    input("Scan QR Code, And then Enter")
+    print("Logged In Whats`Up")
+    # Set phone number
+    print("start parse WhatsUp")
+    for phon in phones:
+        try:
+            inp_xpath_search = "//div[@title='Текстовое поле поиска']"
+            input_box_search = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(inp_xpath_search))
+            input_box_search.click()
+            time.sleep(1)
+            for x in range(12):
+                input_box_search.send_keys(Keys.BACK_SPACE)
+            input_box_search.send_keys(phon)
+            # Click in chats
+            time.sleep(1)
+            chats = "//div[@data-testid='cell-frame-container']"
+            selected_contact = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(chats))
+            chats = "//div[@data-testid='cell-frame-container']"
+            selected_contact = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(chats))
+            selected_contact.click()
+            # click person info
+            time.sleep(1)
+            pers = "//div[@data-testid='conversation-info-header']"
+            selected_contact = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(pers))
+            selected_contact.click()
+            # get Image
+            time.sleep(1)
+            icon = "//img[@class='_8hzr9 M0JmA i0jNr']"
+            selected_contact = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath(icon))
+            url = selected_contact.get_attribute("src")
+            resp = req.get(url)
+            with open("images/WhastUp/"+phon+".jpg", "wb") as file:
+                file.write(resp.content)
+        except:
+            pass
+    driver.quit()
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 from ParseVCF import VcardFile
-#import WhatsUp
+import WhatsUp
+
+def makephonelist():
+    phones = []
+    with open("vcard/phones.csv") as file:
+        file.readline()
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            line = line.split(",")
+            if line[0][0] == "+":
+                phones.append(line[0].replace(" ", ""))
+        return phones
+makephonelist()
 
 if __name__ == '__main__':
     vcf = VcardFile()
     print("from vCard csv - 1; from csv to vCard - 2")
-    opencard = input(1)
+    opencard = input()
     if opencard == 1:
         vcf.openvcard("Contacts.vcf")
         vcf.saveimage()
         vcf.savetocsv()
     if opencard == 2:
         vcf.savevcard("Contacts_new.vcf")
+    if opencard == "3":
+        WhatsUp.connectwp(makephonelist())
     else:
         print("bad number")
     print('compete')
